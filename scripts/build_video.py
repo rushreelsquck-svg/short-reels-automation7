36fec86cddfc32f04bd099163c9d4617f9638e77
latojav3-oss <latojav3@gmail.com>
2026-07-13 16:59:18 +0800

    Increase music volume from 0.10 to 0.18

--- scripts/build_video.py
+++ scripts/build_video.py
@@ -226,7 +226,7 @@ def build_video(scenes, output_path):
     music_files = list(MUSIC_DIR.glob("*.mp3"))
     if music_files:
         music = AudioFileClip(str(random.choice(music_files))).fx(afx.audio_loop, duration=total_duration)
-        music = music.fx(afx.volumex, 0.10)
+        music = music.fx(afx.volumex, 0.18)
         audio_tracks.append(music)
 
     final_audio = CompositeAudioClip(audio_tracks).set_duration(total_duration)
